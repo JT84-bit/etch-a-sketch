b107bbfa17f4cc12dad5c7059b6de80f166445cb
Joni <153834656+JT84-bit@users.noreply.github.com>
2024-01-02 21:54:49 +0200

    Add files, draw function

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let screen = document.querySelector("#screen")
+
+function drawGrid (number = 16){
+    for(let i = 1; i <= number; i++){
+        let row = document.createElement("div");
+        for(let i = 1; i <= number; i++){
+            let square = document.createElement('div');
+            square.classList.add('square');
+         
+            row.appendChild(square);
+        }
+
+        screen.appendChild(row);
+    }
+}
+
+
+drawGrid();
