f09cc3771d94613e35bbdece085acf9338155a93
Joni <153834656+JT84-bit@users.noreply.github.com>
2024-01-02 23:42:50 +0200

    Added Reset button

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
-let screen = document.querySelector("#screen")
+let screen = document.querySelector("#screen");
+let reset = document.querySelector(".reset");
+
+reset.addEventListener("click", newSize);
 
 function drawGrid (number = 16){
+    while(screen.firstChild){
+        screen.removeChild(screen.lastChild);
+    }
+
     for(let i = 1; i <= number; i++){
         let row = document.createElement("div");
         for(let i = 1; i <= number; i++){
@@ -15,6 +22,11 @@ function drawGrid (number = 16){
     }
 }
 
+function newSize (){
+    let size = prompt("How many squares per side? (100 max)")
+    size = parseInt(size);
+    drawGrid(size);
+}
 
 drawGrid();
 
