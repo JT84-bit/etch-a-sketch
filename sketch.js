93f89b9163e15d6623f90549f1e5b1854efda117
Joni <153834656+JT84-bit@users.noreply.github.com>
2024-01-02 22:48:26 +0200

    Added header and drawing works

--- sketch.js
+++ sketch.js
@@ -6,13 +6,15 @@ function drawGrid (number = 16){
         for(let i = 1; i <= number; i++){
             let square = document.createElement('div');
             square.classList.add('square');
-         
+            square.onmouseover = function() {
+                this.style.backgroundColor = 'black';
+              };
             row.appendChild(square);
         }
-
         screen.appendChild(row);
     }
 }
 
 
 drawGrid();
+
