f01112e435c466695c6cb6ea5805007955d34c1e
Joni <153834656+JT84-bit@users.noreply.github.com>
2024-01-03 23:10:28 +0200

    Added rest of the buttons and functions for drawin

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
+//All buttons and drawingscreen
 let screen = document.querySelector("#screen");
 let reset = document.querySelector(".reset");
+let black = document.querySelector(".black")
+let colorful = document.querySelector(".colorful") 
 
-reset.addEventListener("click", newSize);
 
+
+//Draws grid of squares on "screen"
 function drawGrid (number = 16){
     while(screen.firstChild){
         screen.removeChild(screen.lastChild);
@@ -14,7 +18,7 @@ function drawGrid (number = 16){
             let square = document.createElement('div');
             square.classList.add('square');
             square.onmouseover = function() {
-                this.style.backgroundColor = 'black';
+                this.style.backgroundColor = "black";
               };
             row.appendChild(square);
         }
@@ -22,11 +26,62 @@ function drawGrid (number = 16){
     }
 }
 
+//On reset asks user for new size for screen
 function newSize (){
-    let size = prompt("How many squares per side? (100 max)")
-    size = parseInt(size);
-    drawGrid(size);
+    let askAgain = true;
+    while(askAgain){
+        let size = prompt("How many squares per side? (100 max)")
+        size = parseInt(size);
+        
+        if(isNaN(size) || size <= 0 || size > 100){
+            alert("Not valid input.")
+            continue;
+        }
+
+        drawGrid(size);
+        askAgain = false;
+    }
+};
+
+
+//Gives random color when using "Colorful" button
+function RGBcolor() {
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+    let randomcolor = "rgb(" + R + "," + G + "," + B + ")";  
+    
+    return randomcolor;
+  }
+
+
+//Gives all squares random colors when "colorful" button is used
+const colorfulMode = () => {
+    
+    let allSquares = document.querySelectorAll(".square")
+    allSquares.forEach(element => {
+        element.onmouseover = function() {
+            this.style.backgroundColor = RGBcolor();
+          };
+    });
 }
+
+
+//Gives all squares black colors when "black" button is used
+const blackMode = () => {
+    let allSquares = document.querySelectorAll(".square")
+    allSquares.forEach(element => {
+        element.onmouseover = function() {
+            this.style.backgroundColor = 'black';
+          };
+    });
+}
+
+
+black.addEventListener("click", blackMode);
+colorful.addEventListener("click", colorfulMode);
+reset.addEventListener("click", newSize);
+
 
 drawGrid();
 
